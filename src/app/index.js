80dbc6a68be66d1711134855118752c69ef24574
gladsonramos <gladsongodinho@hotmail.com>
2024-04-08 23:42:24 -0300

    Cors

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { UsuarioController } from '../controllers/index.js';
 import mongoose from '../database/config.js';
+import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3001; // Define a porta com base na variável de ambiente PORT ou usa a porta 3001 como padrão
-
 app.use(express.json());
+app.use(cors());
 
 app.use('/user', UsuarioController);
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(3001, () => {
+    console.log('Servidor rodando na porta 3001');
 });
